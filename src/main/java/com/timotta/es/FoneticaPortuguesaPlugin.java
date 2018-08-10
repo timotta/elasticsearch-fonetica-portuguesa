fd7ae7ce3c2cbb2e14f4533dcdf041737755884c
Sergio Eduardo Dantas de Oliveira <sergiowww@gmail.com>
2018-08-10 16:41:40 -0300

    mudança da api de log

--- src/main/java/com/timotta/es/FoneticaPortuguesaPlugin.java
+++ src/main/java/com/timotta/es/FoneticaPortuguesaPlugin.java
@@ -2,9 +2,8 @@ package com.timotta.es;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.logging.Logger;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
 import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
 import org.elasticsearch.plugins.AnalysisPlugin;
@@ -14,7 +13,7 @@ import org.elasticsearch.plugins.Plugin;
  */
 public class FoneticaPortuguesaPlugin extends Plugin implements AnalysisPlugin {
 
-	private final static Logger LOG = LogManager.getLogger(FoneticaPortuguesaPlugin.class);
+	private final static Logger LOG = Logger.getLogger(FoneticaPortuguesaPlugin.class.getSimpleName());
 
 	public FoneticaPortuguesaPlugin() {
 		LOG.info("Plugin fonetica portuguesa instanciado");
